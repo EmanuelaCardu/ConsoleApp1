4a4a9d59bdeafe5dd5e1e93f347558522619b4be
Emanuela Cardu <emanuelacardu@gmail.com>
2023-10-12 14:04:45 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int i = 5;
+            long l = i;
+
+            short s = (short)i;
+
+            double d = 3.14;
+            int r = (int)d;
+            
+
+
+
+        }
+    }
+}
